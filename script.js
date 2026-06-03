6c7e9c7cde2400953914127b5e931f0956948533
LeviMeyers <levi.meyers@hotmail.com>
2026-06-03 00:51:51 -0700

    chapter select and unlisted tracks settings complete

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function changeText(button) {
-    button.textContent = "Something else"
+let chapters = [1, 2, 3, 4];
+let unlistedTracks = false;
+
+let mode = "multChoice";
+let difficulty = 1; // 0 = easy; 1 = medium; 2 = hard
+let rounds = 10;
+
+function collectSettings() {
+    const ulTracksInput = document.getElementById("ulTracksToggle")
+    unlistedTracks = ulTracksInput.checked;
+
+    chapters = [];
+    const chapSelectInputs = document.getElementById("chapterList")
+        .getElementsByTagName("input");
+    for (const input of chapSelectInputs) {
+        if (input.checked) {
+            chapters.push(Number(input.parentElement.textContent))
+        }
+    }
+
+    console.log(unlistedTracks);
+    console.log(chapters);
+}
+
+function maintainChecklist(checklistId, currentCheckbox) {
+    const checklist = document.getElementById(checklistId)
+        .getElementsByTagName("input");
+
+    let checkedItems = 0;
+    for (const input of checklist) {
+        if (input.checked) {
+            checkedItems++;
+        }
+    }
+
+    // // if this is the last checked item in the list
+    if (checkedItems < 1) {
+        currentCheckbox.checked = true;
+    }
 }
